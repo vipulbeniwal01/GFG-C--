70ca21c2a32dd583a95692c14fd8a3113fb56a96
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-03 20:30:26 +0530

    Scope Of Variables

---
+++ ScopeOfVariables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int x;
+int main(){
+    // int x;
+    cout<<x;
+    return 0;
+}
+//undefined local variables takes garbage value whereas on the other hand undefined global vairable takes the value 0
