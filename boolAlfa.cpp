4501c207208c7bae0f6323fba014c4be04b02325
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-09 07:30:17 +0530

    Important

---
+++ boolAlfa.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    cout<<a<<endl;
+    cout<<boolalpha;
+    cout<<a<<endl;
+    cout<<noboolalpha;
+    cout<<a<<endl;
+    return 0;
+}
