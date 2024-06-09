da960f85de4a7d813dafc426a87bedf98cd2d7f2
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-09 11:36:05 +0530

    default precision of float is 6

--- printFloat.cpp
+++ printFloat.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main(){
-    float a = 128732.34;
-    cout<<a;
+    float a = 1287232.34;
+    cout<<a<<endl;
+    cout<<setprecision(4);
+    cout<<a<<endl;
+    cout<<showpoint;
+    cout<<a<<endl;
+    cout<<showpos;
+    cout<<a<<endl;
+    cout<<uppercase;
+    cout<<a<<endl;
 }
