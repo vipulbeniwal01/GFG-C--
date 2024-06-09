e0b709a6cdf88f75e73bf94939c47a7e762c4c1d
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-09 11:54:16 +0530

    Basic Comparison Operator

---
+++ comparisonOperator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int x = 2, y = 6;
+    cout<<(x>y)<<endl;
+    cout<<(x<y)<<endl;
+    cout<<boolalpha;
+    cout<<(x>y)<<endl;
+    cout<<(x<y)<<endl;
+}
