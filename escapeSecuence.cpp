2a2b4de8609900e945e8c19f61b344c0be7f1806
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-09 07:38:06 +0530

    Basic Codes

---
+++ escapeSecuence.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Welcome to the repositry of \"C++\""<<endl;
+    return 0;
+}
---
+++ unknowSecuenceCharacter.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Vipul \ Beniwal"<<endl;
+    return 0;
+}
+//this code generare output but with output it also generates warning
