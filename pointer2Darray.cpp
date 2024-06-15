b16cfafc2b1ed6a3216d730952bbdd72aace0b26
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-15 08:34:38 +0530

    code

---
+++ pointer2Darray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m, n;
+    cin>>m>>n;
+    int **arr;
+    arr = new int*[m];
+    for(int i=0; i<m; i++){
+        arr[i] = new int[n];
+    }
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            arr[i][j]=10;
+            cout<<arr[i][j]<<" ";
+        }
+    }
+}
